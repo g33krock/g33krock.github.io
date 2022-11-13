ea3aecb64a9f870a65ef423c881365ba8c67ecfe
Dallas Lovell <dallas.lovell@gmail.com>
2022-11-13 12:21:23 -0700

    LinkedIn Tag

--- titles.js
+++ titles.js
@@ -1,8 +1,8 @@
 const titles = ["Full Stack Web Developer", "Software Engineer", "Code Debugger", "Problem Solver", "Solutions Creator"]
 let title
 
-const index = 0;
+let index = 0;
 setInterval(function(){
     title = (titles[index++ % titles.length]);
     document.getElementById("titleHolder").innerHTML = title.toString()
-}, 1000)
+}, 5000)
