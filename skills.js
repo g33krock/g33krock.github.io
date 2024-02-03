57b3c10a46870ba5f95f32f362c277e525264f5f
g33krock <dallas.lovell@gmail.com>
2024-02-03 15:42:58 -0700

    character sheet

---
+++ skills.js
@@ -0,0 +1,66 @@
+export const skills = {
+    "Physical Prowess": {
+        traits: [
+            {name: "Acrobatics", description: "Mastery of agility, balance, and physical coordination for navigating challenging environments."},
+            {name: "Melee Combat", description: "Proficiency in close-quarter combat using physical strength and dexterity."},
+            {name: "Marksmanship", description: "Precision and accuracy in the use of ranged weapons, showcasing physical prowess and hand-eye coordination."},
+            {name: "Piloting", description: "Expertise in controlling and maneuvering vehicles, spacecraft, or mechs, showcasing physical prowess in handling complex machinery."},
+            {name: "Athletics", description: "General physical fitness, including strength, endurance, and overall athleticism."},
+        ],
+        enhancements: [
+            {name: "Cybernetic Limbs", credits: 50000, bonus: 50, description: "Sleek metallic limbs with smooth articulation, enhancing strength or dexterity."},
+            {name: "Invisibility Cloaking", credits: 120000, bonus: 120, description: "A subtle distortion field, rendering you nearly invisible to the naked eye."},
+            {name: "Enhanced Reflexes", credits: 45000, bonus: 45, description: "Neural enhancements visible as faint tracings along your limbs, improving reaction time and precision."},
+            {name: "Gravity Nullifiers", credits: 85000, bonus: 85, description: "Advanced implants that nullify the effects of gravity, allowing the user to navigate in low-gravity environments effortlessly."},
+            {name: "Magnetic Grapple Fingers", credits: 110000, bonus: 110, description: "Robotic fingers with embedded magnetic grapple technology that allows the user to cling to metallic surfaces, scale vertical structures, and execute precise acrobatic maneuvers. The magnetic field can be adjusted for varying degrees of grip strength."},
+        ]
+    },
+    "Mental Acuity": {
+        traits: [
+            {name: "Hacking/Programming", description: "Proficiency in manipulating and navigating computer systems, demonstrating intelligence and problem-solving skills."},
+            {name: "Science/Research", description: "Aptitude for scientific inquiry and research, requiring intellectual prowess and analytical thinking."},
+            {name: "Mechanical Engineering", description: "Skill in designing, building, and repairing technological devices, showcasing both intelligence and practical problem-solving."},
+            {name: "Tactics/Strategy", description: "Ability to formulate and execute strategic plans, requiring keen intellect and foresight."},
+            {name: "Investigation", description: "Aptitude for uncovering information, solving mysteries, and analyzing data."},
+        ],
+        enhancements: [
+            {name: "Neural Implants", credits: 75000, bonus: 75, description: "Microscopic implants enhancing cognitive abilities, visible only through subtle neural patterns on the temples."},
+            {name: "Augmented Vision", credits: 40000, bonus: 40, description: "Cybernetic eyes with a faint glow, offering night vision, infrared capabilities, and augmented reality overlays."},
+            {name: "Mind-Computer Interface (MCI)", credits: 80000, bonus: 80, description: "Subtle neural connectors, allowing direct communication with computers and external device control through thought."},
+            {name: "Mood Controlled Biofeedback", credits: 30000, bonus: 30, description: "Internal biometric regulators, allowing mood regulation for improved mental well-being and focus."},
+            {name: "Memory Matrix Implants", credits: 95000, bonus: 95, description: "Nanotech-based implants that enhance memory capacity and recall speed, improving cognitive abilities."},
+        ]
+    },
+    "Vitality": {
+        traits: [
+            {name: "Endurance", description: "Stamina and resilience to physical strain and exhaustion, contributing to overall vitality."},
+            {name: "Medical Knowledge", description: "Understanding of anatomy, physiology, and medical procedures for treating injuries and illnesses."},
+            {name: "Survival", description: "Skill in navigating and thriving in harsh environments, showcasing vitality and adaptability."},
+            {name: "Environmental Adaptation", description: "Ability to endure extreme conditions, such as low gravity, high radiation, or toxic atmospheres."},
+            {name: "First Aid", description: "Basic medical skills for providing immediate care in emergency situations, contributing to vitality."},
+        ],
+        enhancements: [
+            {name: "Biomechanical Heart", credits: 60000, bonus: 60, description: "A pulsating biomechanical heart visible beneath the chest, providing improved cardiovascular performance."},
+            {name: "Subdermal Armor", credits: 70000, bonus: 70, description: "Subtle shimmering beneath the skin, offering enhanced protection akin to an invisible exoskeleton."},
+            {name: "Nano-enhancements", credits: 90000, bonus: 90, description: "Nanobots coursing through your veins, offering accelerated healing and increased immunity."},
+            {name: "Synthetic Organs", credits: 65000, bonus: 65, description: "Internal synthetic organs visible through translucent skin, providing improved functionality and potential longevity."},
+            {name: "Energy Absorption Cells", credits: 110000, bonus: 110, description: "Microscopic cells visible beneath the skin, allowing absorption and conversion of various energy forms."},
+        ]
+    },
+    "Social Influence": {
+        traits: [
+            {name: "Negotiation/Diplomacy", description: "Ability to influence others through persuasion, negotiation, and diplomatic skills."},
+            {name: "Leadership", description: "Skill in guiding and motivating a group, showcasing the ability to influence and inspire."},
+            {name: "Persuasion", description: "Convincing others to see and accept your point of view through charm and rhetoric."},
+            {name: "Intimidation", description: "Using presence and assertiveness to instill fear or compliance in others."},
+            {name: "Etiquette/Protocol", description: "Understanding and mastery of social norms and protocols, contributing to effective social interactions."},
+        ],
+        enhancements: [
+            {name: "Holographic Interface", credits: 50000, bonus: 50, description: "A holographic display shimmering in front of your eyes, enabling advanced communication and data visualization."},
+            {name: "Biometric Camouflage", credits: 55000, bonus: 55, description: "Dynamic skin color shifts, allowing effective disguise and blending into surroundings."},
+            {name: "Telepathic Communication Implants", credits: 100000, bonus: 100, description: "Subtle neural nodes on the temples, facilitating communication through thoughts."},
+            {name: "Charm Emitters", credits: 50000, bonus: 50, description: "Subtle devices emitting pheromones and charismatic signals, enhancing the user's ability to influence and persuade others."},
+            {name: "Empathic Resonator", credits: 90000, bonus: 90, description: "Implants that heighten the user's empathic abilities, allowing them to sense and influence the emotions of those around them."},
+        ]
+    }
+}
--- titles.js
+++
@@ -1,8 +0,0 @@
-const titles = ["Full Stack Web Developer", "Software Engineer", "Code Debugger", "Problem Solver", "Solutions Creator"]
-let title
-
-let index = 0;
-setInterval(function(){
-    title = (titles[index++ % titles.length]);
-    document.getElementById("titleHolder").innerHTML = title.toString()
-}, 5000)
