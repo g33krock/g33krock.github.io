99fe1615a9542d3adedb3beba837cbb049450c04
g33krock <dallas.lovell@gmail.com>
2024-04-03 19:33:43 -0700

    organized new files

---
+++ src/engine/logic/buildDecks.mjs
@@ -0,0 +1,23 @@
+import { shuffleArray } from "../../../shuffleDeck.mjs";
+
+
+export function buildInitialDeck(entity, cards) {
+  if (!entity.defaultDeck) return;
+
+  entity.deck = [];
+
+  entity.defaultDeck.forEach((deckItem) => {
+    const { cardName, frequency } = deckItem;
+    const card = cards.find((c) => c.name === cardName);
+
+    if (card) {
+      for (let i = 0; i < frequency; i++) {
+        entity.deck.push({ ...card });
+      }
+    }
+  });
+  entity.deck = shuffleArray(entity.deck);
+  return entity;
+}
+
+// buildInitialDecks(entities, cards);
---
+++ src/engine/objects/cards.mjs
@@ -0,0 +1,412 @@
+export const cards = [
+  {
+    name: "strike",
+    properties: {
+      target: 1,
+      aggro: 2,
+      health: -2,
+    },
+  },
+  {
+    name: "bandage",
+    properties: {
+      target: 0,
+      aggro: 2,
+      health: 3,
+    },
+  },
+  {
+    name: "heal",
+    properties: {
+      target: 1,
+      aggro: 2,
+      health: 2,
+    },
+  },
+  {
+    name: "shield",
+    properties: {
+      target: 2,
+      aggro: 2,
+      shield: 2,
+    },
+  },
+  {
+    name: "grapple",
+    properties: {
+      target: 1,
+      aggro: 3,
+      health: 3,
+    },
+  },
+  {
+    name: "plate armor",
+    properties: {
+      shield: 3,
+      shot: 3,
+      counter: 2,
+      target: 0,
+      aggro: 4,
+    },
+  },
+  {
+    name: "mirror shield",
+    properties: {
+      shield: 1,
+      reflect: 1,
+      counter: 1,
+      target: 0,
+      aggro: 4,
+    },
+  },
+  {
+    name: "rejuvinate",
+    properties: {
+      counter: 3,
+      hot: 2,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "empower",
+    properties: {
+      shield: 1,
+      strengthen: 1,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "weaken",
+    properties: {
+      shield: -1,
+      strengthen: -1,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "poison",
+    properties: {
+      counter: 3,
+      hot: -1,
+      stot: -1,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "venom blade",
+    properties: {
+      health: -2,
+      counter: 2,
+      hot: -1,
+      stot: -1,
+      target: 1,
+      aggro: 3,
+    },
+  },
+  {
+    name: "vanish",
+    properties: {
+      target: 0,
+      aggro: -4,
+    },
+  },
+  {
+    name: "mastermind",
+    properties: {
+      target: 0,
+      aggro: 1,
+    },
+  },
+  {
+    name: "flurry",
+    properties: {
+      target: 1,
+      aggro: 1,
+    },
+  },
+  {
+    name: "explosive poison trap",
+    properties: {
+      target: 0,
+      explosivePoisonTrap: 1,
+      counter: 1,
+      aggro: 3,
+    },
+  },
+  {
+    name: "paralyzing trap",
+    properties: {
+      target: 0,
+      aggro: 3,
+      paralyzingTrap: 1,
+      counter: 1,
+    },
+  },
+  {
+    name: "disengage",
+    properties: {
+      target: 0,
+      aggro: -2,
+    },
+  },
+  {
+    name: "fireball",
+    properties: {
+      health: -5,
+      counter: 1,
+      hot: -1,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "spell block",
+    properties: {
+      target: 1,
+      aggro: 1,
+      interrupt: 1,
+      counter: 1,
+    },
+  },
+  {
+    name: "freeze",
+    properties: {
+      health: -3,
+      shield: 2,
+      counter: 2,
+      stot: -2,
+      target: 1,
+      aggro: 3,
+    },
+  },
+  {
+    name: "illusion",
+    properties: {
+      target: 0,
+      aggro: 3,
+    },
+  },
+  {
+    name: "selfless sacrifice",
+    properties: {
+      health: 5,
+      target: 1,
+      aggro: 2,
+    },
+  },
+  {
+    name: "siphon life",
+    properties: {
+      health: -3,
+      target: 1,
+      aggro: 3,
+    },
+  },
+  {
+    name: "taunt",
+    properties: {
+      target: 0,
+      aggro: 5,
+    },
+  },
+  {
+    name: "slash",
+    properties: {
+      target: 1,
+      health: -3,
+      aggro: 2,
+    },
+  },
+  {
+    name: "evade",
+    properties: {
+      target: 0,
+      shield: 2,
+      aggro: 2,
+    },
+  },
+  {
+    name: "quick attack",
+    properties: {
+      target: 1,
+      shield: -2,
+      health: -1,
+      aggro: 3,
+    },
+  },
+  {
+    name: "regenerate",
+    properties: {
+      target: 0,
+      counter: 3,
+      hot: 2,
+      aggro: 3,
+    },
+  },
+  {
+    name: "block",
+    properties: {
+      target: 0,
+      shield: 2,
+      aggro: 2,
+    },
+  },
+  {
+    name: "rend armor",
+    properties: {
+      target: 1,
+      shield: -5,
+      aggro: 3,
+    },
+  },
+  {
+    name: "scratch",
+    properties: {
+      target: 1,
+      health: -3,
+      shot: -1,
+      counter: 2,
+      aggro: 3,
+    },
+  },
+  {
+    name: "bite",
+    properties: {
+      target: 1,
+      health: -8,
+      aggro: 3,
+    },
+  },
+  {
+    name: "swipe",
+    properties: {
+      target: 2,
+      health: -3,
+      shot: -1,
+      stot: -1,
+      counter: 2,
+      aggro: 5,
+    },
+  },
+  {
+    name: "monster heal",
+    properties: {
+      target: 0,
+      health: 10,
+      aggro: 3,
+    },
+  },
+  {
+    name: "crush",
+    properties: {
+      target: 1,
+      health: -5,
+      shot: -2,
+      counter: 2,
+      aggro: 3,
+    },
+  },
+  {
+    name: "throw rock",
+    properties: {
+      target: 1,
+      health: -10,
+      aggro: 4,
+    },
+  },
+  {
+    name: "stomp",
+    properties: {
+      target: 2,
+      health: -3,
+      shot: -2,
+      stot: -2,
+      counter: 2,
+      aggro: 5,
+    },
+  },
+  {
+    name: "mind blast",
+    properties: {
+      target: 1,
+      health: -5,
+      counter: 1,
+      hot: -3,
+      aggro: 3,
+    },
+  },
+  {
+    name: "psychic scream",
+    properties: {
+      target: 2,
+      health: -5,
+      shot: -1,
+      stot: -1,
+      counter: 2,
+      aggro: 4,
+    },
+  },
+  {
+    name: "debilitate",
+    properties: {
+      target: 2,
+      shot: -1,
+      stot: -2,
+      counter: 3,
+      aggro: 3,
+    },
+  },
+  {
+    name: "reconstitute",
+    properties: {
+      target: 0,
+      counter: 3,
+      hot: 5,
+      aggro: 4,
+    },
+  },
+  {
+    name: "fire breath",
+    properties: {
+      target: 1,
+      health: -10,
+      counter: 1,
+      hot: -5,
+      aggro: 5,
+    },
+  },
+  {
+    name: "tail swipe",
+    properties: {
+      target: 2,
+      health: -8,
+      shot: -2,
+      stot: -2,
+      counter: 2,
+      aggro: 5,
+    },
+  },
+  {
+    name: "wing gust",
+    properties: {
+      target: 2,
+      shot: -3,
+      stot: -3,
+      counter: 3,
+      aggro: 5,
+    },
+  },
+  {
+    name: "fiery regeneration",
+    properties: {
+      target: 0,
+      counter: 3,
+      hot: 10,
+      aggro: 5,
+    },
+  },
+];
