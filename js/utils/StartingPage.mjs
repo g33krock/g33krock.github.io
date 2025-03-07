50d336a83e582a6f8b44872232611c3bc1cf7b23
g33krock <dallas.lovell@gmail.com>
2025-03-07 15:26:37 -0700

    Updated references

--- js/utils/StartingPage.mjs
+++ js/utils/StartingPage.mjs
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const doorImage = document.getElementById("door-image");
 
     doorImage.addEventListener("click", () => {
-        doorImage.src = "../../images/Door/dungeon-door.gif";
+        doorImage.src = "./images/Door/dungeon-door.gif";
         setTimeout(() => {
-            doorImage.src = "../../images/Door/dungeon-door-open.png";
+            doorImage.src = "./images/Door/dungeon-door-open.png";
             setTimeout(() => {
                 doorImage.classList.add("expanded");
                 setTimeout(() => {
-                    window.location.href = "../../playerSelect.html";
+                    window.location.href = "playerSelect.html";
                 }, 400); 
 
             }, 10); 
